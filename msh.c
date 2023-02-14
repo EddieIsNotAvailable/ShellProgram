3bc99895f4d507358e00870654c9dbe73e2df268
EddieIsNotAvailable <throwaway9172@protonmail.com>
2023-02-13 22:55:43 -0600

    Removed debugging and excess comments

--- msh.c
+++ msh.c
@@ -35,23 +35,18 @@
 #include <sys/stat.h>
 
 
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
-                                // so we need to define what delimits our tokens.
-                                // In this case  white space
-                                // will separate the tokens on our command line
-
+#define WHITESPACE " \t\n"
 #define MAX_COMMAND_SIZE 255
 #define MAX_NUM_ARGUMENTS 10
-#define MAX_HISTORY_LENGTH 5
+#define MAX_HISTORY_LENGTH 15
 
 char* history[MAX_HISTORY_LENGTH]={};
-//int sp = -1;
 int historyCount=-1;
 
-void printHistoryWrapped() {
+void printHistory()
+{
   int mod = historyCount % MAX_HISTORY_LENGTH;
   int i=0, count =0, top;
-
   for(i=mod; i>=0; i--)
     printf("%d: %s\n",count++,history[i]);
 
@@ -62,52 +57,10 @@ void printHistoryWrapped() {
     printf("%d: %s\n",count++,history[i]);
 }
 
-void printHistory() {
-  /* printf("History count is %d upon entering\n",historyCount);
-  int height=0;
-  if(MAX_HISTORY_LENGTH > historyCount)
-  {
-    printf("(History count) [%d < %d] (MAX hist len)\n",historyCount,MAX_HISTORY_LENGTH);
-    height = historyCount;
-    printf("So height is %d\n",height);
-
-    for(int i=0; i <= height; i++) {
-      printf("%d: %s\n",i,history[i]);
-    }
-  }
-  else {
-    //printf("History ct is full (%d), max len is %d\n",historyCount,MAX_HISTORY_LENGTH);
-    //printf("printing history wrapped\n\n");
-    printHistoryWrapped();
-  } */
-      printHistoryWrapped();
-
-}
-
-/* void updateHistory(char x[MAX_COMMAND_SIZE]) {
-  for(int i=MAX_HISTORY_LENGTH-1; i>=1; i--) {
-    history[i] = history[i-1];
-  }
-  history[0] = x;
-}
-
-void push(char x[MAX_COMMAND_SIZE]) {
-  if(sp == MAX_HISTORY_LENGTH-1) {
-    updateHistory(x);
-  } else {
-    sp++;
-    for(int i = sp; i>=0; i--) {
-      history[i+1] = history[i];
-    }
-    history[0] = x;
-  }
-} */
-
 void addToHistory(char x[MAX_COMMAND_SIZE])
 {
   strtok(x, "\n");
   history[++historyCount % MAX_HISTORY_LENGTH] = strdup(x);
-  printf("Added %s to history[%d]\n",x, historyCount % MAX_HISTORY_LENGTH);
 }
 
 int getFromHistory(int x)
@@ -139,18 +92,12 @@ int main()
 
   while( 1 )
   {
-    // Print out the msh prompt
+    // Print out the prompt
     printf ("msh> ");
 
-    // Read the command from the commandline.  The
-    // maximum command that will be read is MAX_COMMAND_SIZE
-    // This while command will wait here until the user
-    // inputs something since fgets returns NULL when there
-    // is no input
-
+    //Wait for command to read
     while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
 
-    /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
     for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
@@ -159,16 +106,8 @@ int main()
     }
 
     int token_count = 0;                                 
-                                                           
-    // Pointer to point to the token
-    // parsed by strsep
-    char *argument_ptr = NULL;                                         
-                                                           
+    char *argument_ptr = NULL;                                               
     char *working_string  = strdup( command_string );
-
-    // we are going to move the working_string pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
     char *head_ptr = working_string;
 
     // Tokenize the input strings with whitespace used as the delimiter
@@ -177,40 +116,30 @@ int main()
     {
       token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
       if( strlen( token[token_count] ) == 0 )
-      {
         token[token_count] = NULL;
-      }
-        token_count++;
+      
+      token_count++;
     }
 
-///////////////////////
 
     //Built in command handling
 
     if(token[0] == NULL)
-    {
       continue;
-    }
 
     if(strcmp(token[0],"quit") == 0 || strcmp(token[0],"exit") == 0)
-    {
       exit(0);
-    }
 
     //Check first character of input, then 1 offset for number
     if(*command_string == '!')
     {
       int num = atoi((command_string+1));
-      printf("num was: %d\n",num);
-      //TODO, should run the command in history at the number, or give error for "Command not in history" (at n)
       
+      //Get position in history array of num
       int temp = getFromHistory(num);
-      printf("Got %d from history\n",temp);
       
       for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
-      {
         token[i] = NULL;
-      }
 
       token_count = 0;                                 
       command_string = history[temp];
@@ -223,17 +152,16 @@ int main()
       {
         token[token_count] = strndup( argument_ptr, MAX_COMMAND_SIZE );
         if( strlen( token[token_count] ) == 0 )
-        {
           token[token_count] = NULL;
-        }
-          token_count++;
+        
+        token_count++;
       }
     }
     else addToHistory(command_string);
 
     if(strcmp(token[0], "history") == 0)
     {
-      printHistoryWrapped();
+      printHistory();
       continue;
     }
 
@@ -252,7 +180,6 @@ int main()
     
       if( ret == -1 )
       {
-        //perror("Command not found."); //Use perror for more descriptive error message
         printf("%s: Command not found.\n",token[0]);
         return 0;
       }
